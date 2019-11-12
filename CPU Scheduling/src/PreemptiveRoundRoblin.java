567eeafaef10a32ed0fe7559405864d91e039e68
COYGooner <ivenshao@hotmail.com>
2019-11-11 20:58:59 -0500

    Update PreemptiveRoundRoblin.java

--- CPU Scheduling/src/PreemptiveRoundRoblin.java
+++ CPU Scheduling/src/PreemptiveRoundRoblin.java
@@ -1,10 +1,39 @@
 //C6. Preemptive Round Robin SCheduling (RR)
 public class PreemptiveRoundRoblin extends CPUScheduling{
 
-	@Override
-	public void process() {
-		// TODO Auto-generated method stub
+	List<Row> rows = Utility.deepCopy(this.getRows());
+	int time =0;
+	int timeQuantum = this.getTimeQuantum();
+	
+	while(!rows.isEmpty())
+	{
+		Row row = rows.get(0);
 		
+		if(time<row.getArrivalTime())
+		{
+			time=row.getArrivalTime();
+		}
+		
+		int bt = row.getBurstTime()<timeQuantum ? row.getBurstTime() : timeQuantum);
+		this.getTimeline().add(new Event(row.getProcessName(), time, time + bt));
+		time += bt;
+		rows.remove(0);
+		
+		if(row.geBurstTime()>timeQuantum)
+		{
+			row.setBurstTime(row.getBurstTime()-timeQuantum);
+			for(int i=0;i<rows.size();i++)
+			{
+				if(rows.get(i).getArrivalTime()>time)
+				{
+				rows.add(i,row);
+				break;
+				}
+				else if (i==rows.size()-1)
+				{
+					rows.add(row);
+					break;
+				}
+		}
 	}
-
 }
