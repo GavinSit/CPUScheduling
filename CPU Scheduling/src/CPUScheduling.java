50eab176e2af754d871f625d6645cc75058590ed
COYGooner <ivenshao@hotmail.com>
2019-11-11 21:55:06 -0500

    Update CPUScheduling.java

--- CPU Scheduling/src/CPUScheduling.java
+++ CPU Scheduling/src/CPUScheduling.java
@@ -4,10 +4,15 @@ import java.util.List;
 public abstract class CPUScheduling {
 	private List<Process> processes; // stores the processes that will happen
 	private List<Event> event; // stores the times that the CPU processes the queued processes
-
+	private final List<Row> rows;
+	private final List<Event> timeline;
+	private int timeQuantum;
+	
 	public CPUScheduling() {
 		processes = new ArrayList<Process>();
 		event = new ArrayList<Event>();
+		rows = new ArrayList();
+		timeQuantum = 1;
 	}
 
 	public abstract void process();
@@ -15,6 +20,12 @@ public abstract class CPUScheduling {
 	public void add(Process P) { // add a process to list
 		processes.add(P);
 	}
+	
+	public boolean add(Row row)
+   	{
+    	    return rows.add(row);
+    	}
+	
 
 	public double averageWaitingTime() {
 		double sum = 0;
@@ -46,4 +57,36 @@ public abstract class CPUScheduling {
 		return event;
 	}
 
+	public Row getRow(String process)
+    	{
+        	for (Row row : rows)
+        {
+            if (row.getProcessName().equals(process))
+            {
+                return row;
+            }
+        }
+        
+        	return null;
+    	}
+    
+    	public List<Row> getRows()
+    	{
+        	return rows;
+    	}
+    
+    	public List<Event> getTimeline()
+    	{
+        	return timeline;
+    	}
+	
+	public void setTimeQuantum(int timeQuantum)
+    	{
+        	this.timeQuantum = timeQuantum;
+    	}
+    
+    	public int getTimeQuantum()
+    	{
+        	return timeQuantum;
+    	}
 }
